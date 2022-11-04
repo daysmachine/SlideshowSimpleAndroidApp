7615dd7ae0fcc2ca68f6e0c8a60672505095eab7
Daisy Catalan <dcatalan@csu.fullerton.edu>
2022-11-03 17:08:40 -0700

    progress

--- app/src/main/java/com/example/slideshow_simpleandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/slideshow_simpleandroidapp/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.slideshow_simpleandroidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var prevButton: Button
+    private lateinit var nextButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        //stuff to leave be
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //let's link our Button variables up to the real Button Views
+        this.prevButton = this.findViewById(R.id.button_previous)
+        this.nextButton = this.findViewById(R.id.button_next)
+
+        //Setup action listeners for our buttons!
+        this.prevButton.setOnClickListener { view: View ->
+            println("Previous button clicked!")
+        }
+
+        this.nextButton.setOnClickListener { view: View ->
+            println("Next button clicked!")
+        }
     }
 }
